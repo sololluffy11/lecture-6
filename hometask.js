4880c8eba52234bf14833991affa94f53a2d6278
sololuffy1 <sololevelling12@gmail.com>
2023-11-30 13:41:25 +0300

    first commit

---
+++ hometask.js
@@ -0,0 +1,28 @@
+// function task1(a){
+//     return a.width*a.length*a.height
+// }
+// console.log(task1({width:2,length:5, height:1}));
+
+// function task2(a){
+//     return (a = {
+//         ["Needs"]: Math.floor(a/2),
+//         ["Wants"]: Math.floor(a/3.3333),
+//         ["Savings"]: Math.floor(a/5),
+//     })
+// }
+// console.log(task2(10000));
+
+// function task3(a)
+// {
+//     let cnt;
+//     if (a == "Darth Vader") return "Luke, I am your father.";
+//     if (a == "Leia") return "Luke, I am your sister.";
+//     if (a == "Han") return cnt = "Luke, I am your brother in law.";
+// }
+// console.log(task3("Darth Vader"));
+
+// function task4(a)
+// {
+//     return a.name+" "+"has a population of "+a.population+" "+"and is situated in "+a.continent;
+// }
+// console.log(task4({ name: "Tokyo", population: "13,929,286", continent: "Asia" }));
---
+++ main.js
@@ -0,0 +1,48 @@
+// let person = {
+//     "full name": "Muhammad",
+//      2:20,
+//      surname: "Khishov",
+// };
+// console.log(person.surname);
+
+// let person = {
+//     "full name": "Muhammad",
+//      2:20,
+//      surname: "Khishov",
+// };
+// console.log(person["full name"]);
+
+// let person = {
+//     "full name": "Muhammad",
+//      2:20,
+//      surname: "Khishov",
+// };
+
+// person["names"]= "John" 
+// console.log(person);
+
+// let person = {
+//         "full name": "Muhammad",
+//          2:20,
+//          surname: "Khishov",
+//     };
+    
+//     person[2]=30
+//     console.log(person);
+
+// let person = {
+//         "full name": "Muhammad",
+//          2:20,
+//          surname: "Khishov",
+//     };
+    
+//     delete person["surname"]
+//     console.log(person);
+
+// let person = {
+// "full name": "Muhammad",
+// 2:20,
+// surname: "Khishov",
+// };
+// let {surname} = person 
+// console.log(surname);
